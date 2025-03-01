5920670e8a7f44a66c5625af55ac1fc3b359e39e
Martin Nordholts <martin.nordholts@codetale.se>
2025-03-01 23:16:36 +0100

    wip  2025-03-01T23:16:36+01:00

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ async fn main() {
         // `GET /` goes to `root`
         .route("/", get(root))
         // `POST /users` goes to `create_user`
-        .route("/users", post(create_user));
+        .route("/websockets", post(create_user));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -31,9 +31,9 @@ async fn main() {
 
 // basic handler that responds with a static string
 async fn root() -> &'static str {
-    "Hello, World!"
+    "pong!!"
 }
-#[axum::debug_handler]
+
 async fn create_user(
     // this argument tells axum to parse the request body
     // as JSON into a `CreateUser` type
