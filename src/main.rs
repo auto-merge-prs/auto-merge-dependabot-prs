8cbcd68b0a73463fe8cb4e188c1bbacb78ae7e62
Martin Nordholts <martin.nordholts@codetale.se>
2025-03-05 06:46:43 +0100

    wip  2025-03-05T06:46:43+01:00

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,10 @@
-// Use this code snippet in your app
-// If you need more information about configurations or implementing the sample code, visit the AWS docs:
-// https://docs.aws.amazon.com/sdk-for-rust/latest/dg/getting-started.html
-
-use aws_config::{self, BehaviorVersion, Region};
-use aws_sdk_secretsmanager;
-
-use axum::{
-    routing::{get, post},
-    http::StatusCode,
-    Json, Router,
-};
-use serde::{Deserialize, Serialize};
+use lambda_http::{run, service_fn, tracing, Error};
+mod http_handler;
+use http_handler::function_handler;
 
 #[tokio::main]
-async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+async fn main() -> Result<(), Error> {
+    tracing::init_default_subscriber();
 
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/webhook", post(handle_webhook));
-
-    // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "pong!!"
-}
-
-async fn handle_webhook(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> (StatusCode, Json<User>) {
-    // insert your application logic here
-    let user = User {
-        id: 1337,
-        username: payload.username,
-    };
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    id: u64,
-    username: String,
-}
-
-// #[::tokio::main]
-async fn main2() -> Result<(), aws_sdk_secretsmanager::Error> {
-    let secret_name = "auto-merge-dependabot-pull-requests-private-key-1";
-    let region = Region::new("eu-west-1");
-
-    let config = aws_config::defaults(BehaviorVersion::v2023_11_09())
-        .region(region)
-        .load()
-        .await;
-
-    let asm = aws_sdk_secretsmanager::Client::new(&config);
-
-    let response = asm
-        .get_secret_value()
-        .secret_id(secret_name)
-        .send()
-        .await?;
-    // For a list of exceptions thrown, see
-    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-
-    let secret_string = response.secret_string();
-
-    // Your code goes here
-
-    Ok(())
+    run(service_fn(function_handler)).await
 }
