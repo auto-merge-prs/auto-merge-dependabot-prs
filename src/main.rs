7f7326f2122fd82bc26f64c67fb676ed7907ebdf
Martin Nordholts <martin.nordholts@codetale.se>
2025-03-15 10:08:07 +0100

    wip  2025-03-15T10:08:07+01:00

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,12 @@ use lambda_http::{Body, Request, RequestExt, Response};
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+    // // Extract some useful information from the request
+    // let who = event
+    //     .query_string_parameters_ref()
+    //     .and_then(|params| params.first("name"))
+    //     .unwrap_or("world");
+    // let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
 
     dbg!("NORDH", event);
 
@@ -24,7 +24,7 @@ pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, E
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
-        .body(message.into())
+        .body("message.into()".into())
         .map_err(Box::new)?;
     Ok(resp)
 }
