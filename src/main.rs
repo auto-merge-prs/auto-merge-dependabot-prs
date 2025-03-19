aca29f463262e97b173d35f9ee8693f718ae0e24
Martin Nordholts <martin.nordholts@codetale.se>
2025-03-19 08:18:49 +0100

    wip  2025-03-19T08:18:49+01:00

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,13 @@ mod http_handler;
 
 // /home/martin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lambda_runtime-0.13.0/src/deserializer.rs:const ERROR_CONTEXT: &str = "failed to deserialize the incoming data into the function's payload type";
 
-// https://od5ozeeh5gdawbtn.nordholts.se/webhook
-
 use lambda_http::{Body, Request, RequestExt, Response};
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+pub(crate) async fn function_handler2(event: Request) -> Result<Response<Body>, Error> {
     // // Extract some useful information from the request
     // let who = event
     //     .query_string_parameters_ref()
@@ -29,6 +27,30 @@ pub(crate) async fn function_handler(event: Request) -> Result<Response<Body>, E
         .body("ill get back to you nordh".into())
         .map_err(Box::new)?;
     Ok(resp)
+}
+
+#[tokio::main]
+async fn main2() -> Result<(), Error> {
+    tracing::init_default_subscriber();
+
+    run(service_fn(function_handler)).await
+}
+
+use lambda_runtime::{service_fn, LambdaEvent, Error};
+use serde_json::{json, Value};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = service_fn(func);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
+    let (event, _context) = event.into_parts();
+    let first_name = event["firstName"].as_str().unwrap_or("world");
+
+    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
 }
 
 #[cfg(test)]
@@ -72,13 +94,4 @@ mod tests {
             "Hello auto-merge-dependabot-pull-requests-webhook, this is an AWS Lambda HTTP request"
         );
     }
-}
-
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
-
-    eprintln!("NORDH 20");
-    run(service_fn(function_handler)).await
 }
