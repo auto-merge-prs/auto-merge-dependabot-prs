7fdb38dc5376ef0827e63615f6978c1419e0b35f
Martin Nordholts <martin.nordholts@codetale.se>
2025-03-19 13:32:37 +0100

    wip  2025-03-19T13:32:37+01:00

--- src/signature.rs
+++ src/signature.rs
@@ -12,22 +12,30 @@ impl fmt::Display for SignedPayloadError {
 
 impl std::error::Error for SignedPayloadError {}
 
+#[derive(Eq, PartialEq, Debug)]
 enum VerificationResult {
     Success,
     Failure,
 }
 
-pub fn verify_signature(signature_header_value: &str, payload: &[u8], secret: &str) -> VerificationResult {
-    let expected_signature = signature_header_value["sha256=".len()..];
-    let expected_signature = hex::decode(expected_signature).unwrap();
-    let mut signer = Signer::new(MessageDigest::sha256(), secret).unwrap();
+pub fn verify_signature(
+    signature_header_value: &str,
+    secret: &str,
+    payload: &[u8],
+) -> VerificationResult {
+    let expected_signature = hex::decode(&signature_header_value["sha256=".len()..]).unwrap();
+    let mut signer = Signer::new(
+        MessageDigest::sha256(),
+        &PKey::hmac(secret.as_bytes()).unwrap(),
+    )
+    .unwrap();
     signer.update(payload).unwrap();
     let actual_signature = signer.sign_to_vec().unwrap();
     return if memcmp::eq(&actual_signature, &expected_signature) {
         VerificationResult::Success
     } else {
         VerificationResult::Failure
-    }
+    };
 }
 
 #[cfg(test)]
@@ -36,19 +44,13 @@ mod tests {
 
     #[test]
     fn test_github_example() {
-        assert_eq!(VerificationResult::Success, verify_signature("sha256=757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17", "It's a Secret to Everybody"))
-
-        ng secret and payload values to verify that your implementation is correct:
-
-        secret: 
-        payload: Hello, World!
-    
-    If your implementation is correct, the signatures that you generate should match the following signature values:
-    
-        signature: 757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17
-        X-Hub-Signature-256: 
-    
-    
-        
+        assert_eq!(
+            VerificationResult::Success,
+            verify_signature(
+                "sha256=757107ea0eb2509fc211221cce984b8a37570b6d7586c22c46f4379c8b043e17",
+                "It's a Secret to Everybody",
+                "Hello, World!".as_bytes(),
+            )
+        );
     }
 }
