5661a457949188f97d6a83ad6f5db82bc0b67521
Martin Nordholts <martin.nordholts@codetale.se>
2025-03-19 09:49:35 +0100

    wip  2025-03-19T09:49:35+01:00

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
-use lambda_http::{run, service_fn, tracing, Error};
+use lambda_http::{service_fn, Error};
 mod http_handler;
-use lambda_http::{Body, Request, RequestExt, Response};
 use lambda_runtime::LambdaEvent;
-use serde_json::{json, Value};
+use octocrab::models::{events::payload::PullRequestEventPayload, webhook_events::WebhookEvent};
+
+async fn handle_webhook_event(
+    lambda_event: LambdaEvent<WebhookEvent>,
+) -> Result<String, Error> {
+    match lambda_event.into_parts().0.specific {
+        octocrab::models::webhook_events::WebhookEventPayload::PullRequest(pull_request_webhook_event_payload) => todo!(),
+        _ => return Ok("TODO: More specific error"),
+    }
+    let (event, _context) = ;
+    let pr = event.specific;
+    Ok(format!("Got {:?} from {:?} ({:?}).", event.action, pr));
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let func = service_fn(func);
+    let func = service_fn(handle_pull_request_event);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
-async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
-    let (event, _context) = event.into_parts();
-    let first_name = event["firstName"].as_str().unwrap_or("world");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    Ok(json!({ "message": format!("Hello, {}!", first_name) }))
+    #[test]
+    fn test_pull_request_opened_by_dependabot() {
+        let payload = include_str!("../tests/webhook-request-examples/pull-request-opened-by-dependabot/payload.json");
+        let headers = include_str!("../tests/webhook-request-examples/pull-request-opened-by-dependabot/headers.txt");
+        let request 
+        
+    }
 }
