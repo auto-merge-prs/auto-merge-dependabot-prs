d3b5f845dfce90cee3a0013be7c26b1eff5c2ed4
Martin Nordholts <martin.nordholts@codetale.se>
2025-03-19 08:38:55 +0100

    wip  2025-03-19T08:38:55+01:00

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,6 @@
 use lambda_http::{run, service_fn, tracing, Error};
 mod http_handler;
-
-// /home/martin/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lambda_runtime-0.13.0/src/deserializer.rs:const ERROR_CONTEXT: &str = "failed to deserialize the incoming data into the function's payload type";
-
 use lambda_http::{Body, Request, RequestExt, Response};
-
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-pub(crate) async fn function_handler2(event: Request) -> Result<Response<Body>, Error> {
-    // // Extract some useful information from the request
-    // let who = event
-    //     .query_string_parameters_ref()
-    //     .and_then(|params| params.first("name"))
-    //     .unwrap_or("world");
-    // let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
-
-    dbg!("NORDH", event);
-
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/plain")
-        .body("ill get back to you nordh".into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
-
-#[tokio::main]
-async fn main2() -> Result<(), Error> {
-    tracing::init_default_subscriber();
-
-    run(service_fn(function_handler2)).await
-}
-
 use lambda_runtime::LambdaEvent;
 use serde_json::{json, Value};
 
@@ -51,49 +16,4 @@ async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let first_name = event["firstName"].as_str().unwrap_or("world");
 
     Ok(json!({ "message": format!("Hello, {}!", first_name) }))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use lambda_http::{Request, RequestExt};
-    use std::collections::HashMap;
-
-    #[tokio::test]
-    async fn test_generic_http_handler() {
-        let request = Request::default();
-
-        let response = function_handler2(request).await.unwrap();
-        assert_eq!(response.status(), 200);
-
-        let body_bytes = response.body().to_vec();
-        let body_string = String::from_utf8(body_bytes).unwrap();
-
-        assert_eq!(
-            body_string,
-            "Hello world, this is an AWS Lambda HTTP request"
-        );
-    }
-
-    #[tokio::test]
-    async fn test_http_handler_with_query_string() {
-        let mut query_string_parameters: HashMap<String, String> = HashMap::new();
-        query_string_parameters.insert(
-            "name".into(),
-            "auto-merge-dependabot-pull-requests-webhook".into(),
-        );
-
-        let request = Request::default().with_query_string_parameters(query_string_parameters);
-
-        let response = function_handler2(request).await.unwrap();
-        assert_eq!(response.status(), 200);
-
-        let body_bytes = response.body().to_vec();
-        let body_string = String::from_utf8(body_bytes).unwrap();
-
-        assert_eq!(
-            body_string,
-            "Hello auto-merge-dependabot-pull-requests-webhook, this is an AWS Lambda HTTP request"
-        );
-    }
 }
